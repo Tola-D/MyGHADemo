33ff8a9ff65379abf308b754b3d43f26ccfc9d81
Tola-D <144619348+Tola-D@users.noreply.github.com>
2024-09-23 11:08:29 +0100

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -5,7 +5,7 @@ import glob
 import os
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test split
+from sklearn.model_selection import train_test_split
 
 
 # define functions
