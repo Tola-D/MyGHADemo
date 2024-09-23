e5b7523e247a732a48d3c97c4224827d016a21c6
Tola-D <144619348+Tola-D@users.noreply.github.com>
2024-09-23 12:06:58 +0100

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -1,4 +1,4 @@
-# Import libraries
+l# Import libraries
 
 import argparse
 import glob
@@ -44,7 +44,8 @@ def split_data(df):
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    model = LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    return model
 
 
 def parse_args():
